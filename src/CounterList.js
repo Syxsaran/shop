d205bcc93566d7014d51939c7c49597245f687a9
Syxsaran <96071669+Syxsaran@users.noreply.github.com>
2023-09-05 17:07:47 +0700

    1.2
    1.2

--- src/CounterList.js
+++ src/CounterList.js
@@ -2,30 +2,42 @@ import { Component } from "react";
 import Counter from "./Counter";
 
 class CounterList extends Component{
+    
     state = {
         itemlist:[
-            {id: 1, value: 10, itemname:"iPhone" , price:150 },
-            {id: 2, value: 20, itemname:"iPad", price:200 },
-            {id: 3, value: 30, itemname:"iMac", price:300 },
-            {id: 4, value: 40, itemname:"iBook", price:500 },
+            {id: 1, value: 0, itemname:"iPhone" , price:150 },
+            {id: 2, value: 0, itemname:"iPad", price:200 },
+            {id: 3, value: 0, itemname:"iMac", price:300 },
+            {id: 4, value: 0, itemname:"iBook", price:500 },
         ]
     };
+
+    
     render () {
         return (
-            <div>
-                <div className="d-inline mx-2">ราคารวม 100</div>
-                <button 
-                    onClick={this.resetButton}
-                    className="btn btn-sm btn-warning d-inline">Reset</button>               
-                {
-                    this.state.itemlist.map((item) => {                        
-                        return (<Counter key={item.id} item={item} onIncrement={this.Increment} />);
-                        }
-                    )
-                    
-                }
+            <div className="card" style={{ width: "18rem" }}>
+                <ul className="list-group list-group-flush">
+                    <div className="d-inline-flex align-items-center mx-2">
+                        <li className="list-group-item"> ราคารวม : {this.getTotalPrice()}</li>
 
-            </div>    
+                        <button
+                        onClick={this.resetButton}
+                        className="btn btn-sm btn-warning d-inline"
+                        > Reset </button>
+                        </div> 
+                            
+                        {this.state.itemlist.map((item) => (
+                            <li className="list-group-item" key={item.id}>
+                                <Counter
+                                    key={item.id}
+                                    item={item}
+                                    onIncrement={this.Increment}
+                                    ondecrement={this.decrement}/>
+                                </li>
+                            ))
+                        }
+                </ul>
+            </div>
         );
     }
     
@@ -38,18 +50,27 @@ class CounterList extends Component{
         _itemList[indexItem].value++;
         this.setState({ itemlist : _itemList });
     }
+    
+
+
+    getTotalPrice() {
+        const totalPrice = this.state.itemlist
+          .filter((item) => item.id >= 1 && item.id <= 4)
+          .reduce((sum, item) => sum + item.price * item.value, 0);
+    
+        return totalPrice;
+    }
+
 
     decrement = (item) => {
         var _itemList = [...this.state.itemlist];
         const indexItem = _itemList.indexOf(item);
         _itemList[indexItem] = { ...item };
-        _itemList[indexItem].value--;
-        this.setState({ itemlist : _itemList });
-    }
-
-        
-    //function resetButton() {}
-
+        if (_itemList[indexItem].value > 0) {
+            _itemList[indexItem].value--;
+            this.setState({ itemlist: _itemList });
+        }
+    };
 
     resetButton = () => {
         var resetItem = this.state.itemlist.map((item) => { 
